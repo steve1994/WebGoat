13718a4f4bc144607ea9dc791187bbfce0023bc4
steve1994 <steve.harnadi@gmail.com>
2016-01-31 22:40:26 +0700

    AJAX vulnerabilities : DOM manipulated

--- webgoat-container/src/main/webapp/js/DOMXSS_backup.js
+++ webgoat-container/src/main/webapp/js/DOMXSS_backup.js
@@ -1,5 +1,5 @@
 function displayGreeting(name) {
 	if (name != ''){
-		document.getElementById("greeting").innerHTML="Hello, " + name+ "!";
+		document.getElementById("greeting").innerHTML="Hello, " + escapeHTML(name)+ "!";
 	}
 }
